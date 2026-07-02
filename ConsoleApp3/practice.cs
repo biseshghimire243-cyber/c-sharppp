e76763656a56332ece1dfe8b7a2f42a421e381ac
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-01 21:03:35 -0700

    practice set finishing

---
+++ ConsoleApp3/practice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Rectangle
+{
+    public double Length;
+    public double Breadth;
+
+    
+    public double ComputeArea()
+    {
+        return Length * Breadth;
+    }
+
+   
+    public double ComputePerimeter()
+    {
+        return 2 * (Length + Breadth);
+    }
+}
+
+class Polygon
+{
+    static void Main()
+    {
+        
+        Rectangle rect = new Rectangle();
+
+        Console.Write("Enter Length: ");
+        rect.Length = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Breadth: ");
+        rect.Breadth = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("\nArea = " + rect.ComputeArea());
+        Console.WriteLine("Perimeter = " + rect.ComputePerimeter());
+    }
+}

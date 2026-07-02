7544c046749c8f476ec2ab53fe3f0bcbbed8ef16
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-01 20:28:39 -0700

    method practice program are completed

---
+++ ConsoleApp3/Methodpractice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class methodpractice
+{
+    // Static method for factorial
+    static int Factorial(int n)
+    {
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            fact *= i;
+        }
+
+        return fact;
+    }
+
+    // Non-static method
+    public int Cube(int n)
+    {
+        return n * n * n;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a number for factorial: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Factorial = " + Factorial(num1));
+
+        Console.Write("Enter a number for cube: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        // Create object of the same class
+        methodpractice obj = new methodpractice();
+
+        Console.WriteLine("Cube = " + obj.Cube(num2));
+    }
+}
---
+++ ConsoleApp3/methodpracticesecond.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+
+class methodpractice2
+     
+{
+    static void Main()
+    {
+        
+        Calculator obj = new Calculator();
+
+        Console.Write("Enter first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int result = obj.Add(num1, num2);
+
+        Console.WriteLine("Sum = " + result);
+    }
+}

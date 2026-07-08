8df54c317e828c2d0678c33264dfb8bbdbaa9841
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-07 20:08:32 -0700

    constructor are managed

--- ConsoleApp3/Bank.cs
+++ ConsoleApp3/Bank.cs
@@ -6,12 +6,12 @@ class Bank
     public string? AccountType;
     public double Balance;
 
-    // Default constructor
+    // Default Constructor
     public Bank()
     {
     }
 
-    // Parameterized constructor
+    // Parameterized Constructor
     public Bank(int accountNumber, string accountType, double balance)
     {
         AccountNumber = accountNumber;
@@ -36,15 +36,36 @@ class Bank
         }
     }
 
-    public void SeeBalance()
-    {
-        Console.WriteLine("Account Number: " + AccountNumber);
-        Console.WriteLine("Account Type: " + AccountType);
-        Console.WriteLine("Balance: " + Balance);
-    }
-
     public void Display()
     {
-        SeeBalance();
+        Console.WriteLine("Account Number : " + AccountNumber);
+        Console.WriteLine("Account Type   : " + AccountType);
+        Console.WriteLine("Balance        : " + Balance);
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Bank b1 = new Bank(101, "Saving", 10000);
+        Bank b2 = new Bank();
+
+        b2.AccountNumber = 102;
+        b2.AccountType = "Current";
+        b2.Balance = 15000;
+
+        b1.Deposit(2000);
+        b1.Withdraw(1000);
+
+        b2.Deposit(5000);
+        b2.Withdraw(3000);
+
+        Console.WriteLine("First Account");
+        b1.Display();
+
+        Console.WriteLine("Second Account");
+        b2.Display();
     }
 }
---
+++ ConsoleApp3/defaultconstructor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Student
+{
+    public string Name;
+    public int Age;
+
+    // Default Constructor
+    public Student()
+    {
+        Name = "Bishesh";
+        Age = 23;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Age: " + Age);
+    }
+}
+
+class Default
+{
+    static void Main()
+    {
+        Student s1 = new Student();
+        s1.Display();
+    }
+}

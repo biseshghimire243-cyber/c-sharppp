1a5801b5057c6b18f06f86e2f8b884bd703640e9
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-07 20:36:38 -0700

    inheritance are completed

---
+++ ConsoleApp3/inheritanceeg1.cs
@@ -0,0 +1,49 @@
+﻿//Inheritance in C# – Notes
+//Definition
+
+//Inheritance is an Object-Oriented Programming (OOP) feature that allows one class to inherit the properties(fields) and methods of another class.
+
+//The class whose members are inherited is called the Base Class (Parent Class).
+//The class that inherits is called the Derived Class(Child Class).
+
+//In C#, inheritance is implemented using the : (colon) operator.
+
+//Syntax
+//class Parent
+//{
+//    // Fields and Methods
+//}
+
+//class Child : Parent
+//{
+//    // Additional Fields and Methods
+//}
+//Example 1 (Simple Inheritance)
+using System;
+
+class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Animal is Eating");
+    }
+}
+
+class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("Dog is Barking");
+    }
+}
+
+class AnimalProgram
+{
+    static void Main()
+    {
+        Dog d = new Dog();
+
+        d.Eat();
+        d.Bark();
+    }
+}
---
+++ ConsoleApp3/inheritanceeg2.cs
@@ -0,0 +1,35 @@
+﻿//inheritance eg 2a
+using System;
+
+class HumanInfo
+{
+    public string Name = "Ram";
+    public int Age = 20;
+
+    public void ShowHuman()
+    {
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Age: " + Age);
+    }
+}
+
+class CollegeStudent : HumanInfo
+{
+    public string Faculty = "BSc.CSIT";
+
+    public void ShowFaculty()
+    {
+        Console.WriteLine("Faculty: " + Faculty);
+    }
+}
+
+class InheritanceProgram
+{
+    static void Main()
+    {
+        CollegeStudent s = new CollegeStudent();
+
+        s.ShowHuman();
+        s.ShowFaculty();
+    }
+}

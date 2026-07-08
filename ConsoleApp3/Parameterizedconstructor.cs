9f32be1eec2e11a391320fd97c1fdff7001e1e7e
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-07 20:12:22 -0700

    paarameterized is done

---
+++ ConsoleApp3/Parameterizedconstructor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Studenttt
+{
+    public string Name;
+    public int Age;
+
+    // Parameterized Constructor
+    public Studenttt(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Age: " + Age);
+    }
+}
+
+class Parameterized
+{
+    static void Main()
+    {
+        Studenttt s1 = new Studenttt("Ram", 20);
+        Studenttt s2 = new Studenttt("Hari", 22);
+
+        s1.Display();
+        Console.WriteLine();
+
+        s2.Display();
+    }
+}

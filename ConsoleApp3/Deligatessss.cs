e796671b8dba8a959ef3c97a81b7468e465fac26
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-09 18:40:37 -0700

    the deligates are donne

---
+++ ConsoleApp3/Deligatessss.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// Declare Delegate
+delegate void MyDelegate();
+
+class Demo
+{
+    // Method
+    public static void Message()
+    {
+        Console.WriteLine("Hello! This is Delegate Example.");
+    }
+
+    static void Main()
+    {
+        // Create delegate object
+        MyDelegate d = Message;
+
+        // Call method using delegate
+        d();
+    }
+}

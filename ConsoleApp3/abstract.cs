979126a6662738857849b3b3a1990116b7b4c78d
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-09 19:01:22 -0700

    example are commited

---
+++ ConsoleApp3/abstract.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// Declare Delegate
+delegate void Calculate(int a, int b);
+
+class MathOperation
+{
+    public static void Add(int x, int y)
+    {
+        Console.WriteLine("Sum = " + (x + y));
+    }
+
+    static void Main()
+    {
+        Calculate c = Add;
+
+        c(10, 20);
+    }
+}

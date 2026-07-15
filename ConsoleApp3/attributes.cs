4180e3451c73407817c7f29b0aa8fc0c62288c85
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-14 18:17:04 -0700

    the required things are not taken yet

---
+++ ConsoleApp3/attributes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class StudentAttribute : Attribute
+{
+}
+
+[StudentAttribute]
+class StudentRecord
+{
+}
+
+class CustomAttributeProgram
+{
+    static void Main()
+    {
+        Console.WriteLine("Custom Attribute Example");
+    }
+}

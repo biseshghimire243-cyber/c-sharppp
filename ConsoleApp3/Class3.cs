4b45ce9dd176709eef12020df84da2e0c72d2e85
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-14 18:59:10 -0700

    the others are done also

---
+++ ConsoleApp3/Class3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class TargetExample
+{
+    [Obsolete]
+    public void Show()
+    {
+        Console.WriteLine("Hello from Attribute Target");
+    }
+}
+
+class AttributeTargetProgram
+{
+    static void Main()
+    {
+        TargetExample obj = new TargetExample();
+        obj.Show();
+    }
+}
